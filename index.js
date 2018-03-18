43697bdebf8c8f1f443cae68f007229d09726e7e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 18:58:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function takeANumber(katzDeliLine, name) {
+  for (var i = 1; i < katzDeliLine.length; i++) {
+    return "Welcome, " + name + ". You are number " + i + " in line."
+  }
+}
+
+// function nowServing()
