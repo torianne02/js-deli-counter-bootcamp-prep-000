39bbc6f6d9199a84d4117cc54915a66224c760f5
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 19:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     for (var i = 0; i < katzDeliLine.length; i++) {
       return "Currently serving " + katzDeliLine[0] + ".";
+      katzDeliLine.shift();
     }
-    return katzDeliLine.shift();
   }
   else {
       return "There is nobody waiting to be served!"
