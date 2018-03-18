8f1717fc03358ca994f0e67d873309e84fc67d14
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 19:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     for (var i = 0; i < katzDeliLine.length; i++) {
-      return katzDeliLine[0];
+      return "Currently serving " + katzDeliLine[0] + ".";
     }
   }
   else {
