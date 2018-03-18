ec7941f24af3385ba385d7d59f749f15483eed0c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 19:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
       nextUp += "Currently serving " + katzDeliLine[0] + ".";
       katzDeliLine = katzDeliLine.slice(1, katzDeliLine.length);
-      return katzDeliLine;
+      console.log(katzDeliLine);
   }
   else {
       nextUp += "There is nobody waiting to be served!"
